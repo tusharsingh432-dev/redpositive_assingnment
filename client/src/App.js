fcc904624df2df3d67dfdc5e4f50a31b22529a6b
Tushar Singh <tusharsingh432@gmail.com>
2022-03-12 18:35:06 +0530

    frontEndBase

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Form, Modal } from 'react-bootstrap';
 
 function App() {
+
+
+  //////////////input modal////////////////
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  /////////////input////////////////
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [hobbies, setHobbies] = useState('');
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="App container">
+      <h1 style={{ margin: '10px', "textAlign": "center" }}>DataSet</h1>
+      
+      <div className="row">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Select</th>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Phn No.</th>
+              <th scope="col">Email</th>
+              <th scope="col">Hobbies</th>
+              <th scope="col">Update/Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row"><input type="checkbox" /></th>
+              <td>1</td>
+              <td>Tushar Singh</td>
+              <td>7049088595</td>
+              <td>tusharsingh432@gmail.com</td>
+              <td>Reading</td>
+              <td><i class="fa-light fa-pen-to-square"></i></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div clasName="row" style={{ "display": "flex", justifyContent: 'space-around' }}>
+        <button type='button' className='btn btn-primary col-md-4' onClick={handleShow}>Add</button>
+        <button type='button' className='btn btn-primary col-md-4'>Send</button>
+      </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Entry</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <input type="text" placeholder="Name" className="form-control mb-2" value={name} onChange={(e) => { setName(e.target.value) }} />
+            <input type="number" placeholder="Phone Number" className="form-control mb-2" value={phone} onChange={(e) => { setPhone(e.target.value) }} />
+            <input type="text" placeholder="Email" className="form-control mb-2" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+            <input type="text" placeholder="Hobbies" className="form-control mb-2" value={hobbies} onChange={(e) => { setHobbies(e.target.value) }} />
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -18,4 +18,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
